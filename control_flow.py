7fd56700adbeae194d1c59c4ae367e17336a09a9
Vanitaa Rai <vanit.rai26@gmail.com>
2024-08-13 16:20:15 +0800

    5th control flow qn

--- control_flow.py
+++ control_flow.py
@@ -81,3 +81,21 @@ print(twice_as_large(10, 5))
 # should print False
 print(twice_as_large(11, 5))
 # should print True
+
+# Qn 5
+# Create a function called divisible_by_ten() that 
+# has one parameter named num.
+# The function should return True if num is divisible by 10, 
+# and False otherwise. 
+
+def divisible_by_ten(num):
+  if num % 10 == 0:
+    return True
+  else:
+    return False
+
+
+print(divisible_by_ten(20))
+# should print True
+print(divisible_by_ten(25))
+# should print False
