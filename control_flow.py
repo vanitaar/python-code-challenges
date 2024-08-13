c83169ebaf4bd0b96e7da6351b36e0123bc8d8bb
Vanitaa Rai <vanit.rai26@gmail.com>
2024-08-13 16:18:40 +0800

    4th control flow qn

--- control_flow.py
+++ control_flow.py
@@ -46,6 +46,8 @@ else:
 print("Total: " + str(total))
 print("Is it over budget? " + str(over_budget))
 
+
+# Qn 3
 # Create a function named large_power() that takes 
 # two parameters named base and exponent.
 
@@ -62,3 +64,20 @@ print(large_power(2, 13))
 # should print True
 print(large_power(2, 12))
 # should print False
+
+# Qn 4
+# Create a function named twice_as_large() that 
+# has two parameters named num1 and num2.
+# Return True if num1 is more than double num2. 
+# Return False otherwise.
+
+def twice_as_large(num1, num2):
+  if num1 > (2 * num2):
+    return True
+  else:
+    return False
+
+print(twice_as_large(10, 5))
+# should print False
+print(twice_as_large(11, 5))
+# should print True
