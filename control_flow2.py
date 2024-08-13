a3752f0849c159d4026a370cf10757d62bfac84c
Vanitaa Rai <vanit.rai26@gmail.com>
2024-08-13 16:38:57 +0800

    7th control flow qn

--- control_flow2.py
+++ control_flow2.py
@@ -15,3 +15,19 @@ print(in_range(10, 10, 10))
 # should print True
 print(in_range(5, 10, 20))
 # should print False
+
+# Qn 7
+# Create a function named same_name() that has two parameters 
+# named your_name and my_name.
+# If our names are identical, return True. Otherwise, return False.
+
+def same_name(your_name, my_name):
+  if your_name == my_name:
+    return True
+  else:
+    return False
+
+print(same_name("Colby", "Colby"))
+# should print True
+print(same_name("Tina", "Amber"))
+# should print False
