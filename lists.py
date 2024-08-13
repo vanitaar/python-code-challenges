42ccfa95aee0cfc487d29ecbef4310981f3d056c
Vanitaa Rai <vanit.rai26@gmail.com>
2024-08-13 17:02:25 +0800

    4th & 5th lists qn

--- lists.py
+++ lists.py
@@ -52,3 +52,31 @@ def larger_list(my_list1, my_list2):
 print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10]))
 print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10, 90]))
 print(larger_list([4, 10, 2, 5, 6], [-10, 2, 5, 10, 90]))
+
+# Qn 4
+# Create a function named more_than_n() that has 
+# three parameters named my_list, item, and n.
+
+# The function should return True if item appears in the list more than n times. 
+# The function should return False otherwise.
+
+def more_than_n(my_list, item, n):
+  if my_list.count(item) > n:
+    return True
+  else:
+    return False
+
+print(more_than_n([2, 4, 6, 2, 3, 2, 1, 2], 2, 3)) # True 
+print(more_than_n([2, 4, 6, 2, 3, 2, 1, 3], 2, 3)) # False
+
+# Qn 5 
+# Write a function named combine_sort() that has two parameters named my_list1 and my_list2.
+
+# The function should combine these two lists into one new list and sort the result. 
+# Return the new sorted list.
+
+def combine_sort(my_list1, my_list2):
+  combine = my_list1 + my_list2
+  return sorted(combine)
+
+print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10]))
