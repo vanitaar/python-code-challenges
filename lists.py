4a3c91e8b856011f3581873f5140095f0a47eecc
Vanitaa Rai <vanit.rai26@gmail.com>
2024-08-13 16:59:17 +0800

    3rd lists qn

--- lists.py
+++ lists.py
@@ -36,3 +36,19 @@ def append_sum(my_list):
   return my_list
 
 print(append_sum([1, 1, 2]))
+
+# Qn 3
+# Write a function named larger_list() that has two parameters 
+# named my_list1 and my_list2.
+# The function should return the last element of the list that contains more elements. 
+# If both lists are the same size, then return the last element of my_list1.
+
+def larger_list(my_list1, my_list2):
+  if len(my_list1) == len(my_list2) or len(my_list1) > len(my_list2):
+    return my_list1[-1]
+  else:
+    return my_list2[-1]
+
+print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10]))
+print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10, 90]))
+print(larger_list([4, 10, 2, 5, 6], [-10, 2, 5, 10, 90]))
