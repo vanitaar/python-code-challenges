6b67d33fb7fc6fb69faedd0c7a433389d389c749
Vanitaa Rai <vanit.rai26@gmail.com>
2024-08-13 16:16:56 +0800

    3rd control flow qn

--- control_flow.py
+++ control_flow.py
@@ -46,3 +46,19 @@ else:
 print("Total: " + str(total))
 print("Is it over budget? " + str(over_budget))
 
+# Create a function named large_power() that takes 
+# two parameters named base and exponent.
+
+# If base raised to the exponent is greater than 5000, return True,
+# otherwise return False
+
+def large_power(base, exponent):
+  if base ** exponent > 5000:
+    return True
+  else:
+    return False
+
+print(large_power(2, 13))
+# should print True
+print(large_power(2, 12))
+# should print False
