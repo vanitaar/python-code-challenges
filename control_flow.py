970fc108662150f6fe870a5c518701dcf7a92e23
Vanitaa Rai <vanit.rai26@gmail.com>
2024-08-13 16:13:43 +0800

    2nd control flow qn

--- control_flow.py
+++ control_flow.py
@@ -1,3 +1,4 @@
+# Qn 1
 # You are given two numbers stored in num1 and num2. 
 # If the sum of num1 and num2 is NOT equal to 10, 
 # then store True into a variable called not_ten, 
@@ -14,3 +15,34 @@ else:
   not_ten = False
 
 print("Does the sum of the numbers equal 10? " + str(not_ten))
+
+# Qn 2
+# You are given a monthly budget and some expenses and 
+# need to check if the sum of the expenses goes over budget!
+# First, store the total of all expenses into a variable called total.
+# Next, check if the total is greater than the budget. 
+# If it is, store True into a variable called over_budget, 
+# otherwise store False in over_budget.
+
+# Monthly budget
+budget = 2000
+
+# Monthly expenses
+food_bill = 200
+electricity_bill = 100
+internet_bill = 60
+rent = 1500
+
+# Calculate the total amount of expenses
+total = food_bill + electricity_bill + internet_bill + rent
+
+# Check if the total is greater than the budget and store the result in over_budget
+
+if total > budget:
+  over_budget = True
+else:
+  over_budget = False
+
+print("Total: " + str(total))
+print("Is it over budget? " + str(over_budget))
+
