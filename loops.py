7c8795b0cdc876e7d0f8d511e962096ddbb474a4
Vanitaa Rai <vanit.rai26@gmail.com>
2024-08-14 13:37:05 +0800

    3rd loops qn

--- loops.py
+++ loops.py
@@ -28,3 +28,27 @@ def add_greetings(names):
     return greetings
 
 print(add_greetings(["Owen", "Max", "Sophie"]))
+
+# Qn 3
+# Write a function called delete_starting_evens() that has a parameter named my_list.
+
+# The function should remove elements from the front of my_list until the front of the list is not even. The function should then return my_list.
+
+# For example if my_list started as [4, 8, 10, 11, 12, 15], then delete_starting_evens(my_list) should return [11, 12, 15].
+
+# Make sure your function works even if every element in the list is even!
+
+def delete_starting_evens(my_list):
+    # for num in my_list:
+    #     print("start")
+    #     if num % 2 == 0:
+    #         my_list.pop(0)
+    #         print("popped")
+    while my_list and my_list[0] % 2 == 0:
+        my_list.pop(0)
+    return my_list    
+        
+    
+print(delete_starting_evens([4, 8, 10, 11, 12, 15]))
+print(delete_starting_evens([4, 8, 10]))
+print(delete_starting_evens([]))
