086c68c684238d54f264a46e8dd6832f81f31d74
Vanitaa Rai <vanit.rai26@gmail.com>
2024-08-14 13:39:20 +0800

    3rd loops qn alt sol

--- loops.py
+++ loops.py
@@ -48,7 +48,12 @@ def delete_starting_evens(my_list):
         my_list.pop(0)
     return my_list    
         
-    
+#ALT SOLN
+# def delete_starting_evens(my_list):
+#   while (len(my_list) > 0 and my_list[0] % 2 == 0):
+#     my_list = my_list[1:]
+#   return my_list
+
 print(delete_starting_evens([4, 8, 10, 11, 12, 15]))
 print(delete_starting_evens([4, 8, 10]))
 print(delete_starting_evens([]))
