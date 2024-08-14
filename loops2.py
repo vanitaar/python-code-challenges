454aad910f3493f16224991011b16b87a7c52c16
Vanitaa Rai <vanit.rai26@gmail.com>
2024-08-14 22:38:48 +0800

    10th loops qn

--- loops2.py
+++ loops2.py
@@ -100,3 +100,32 @@ print(same_values([1, 2, 3], [4, 5, 6])) #[]
 print(same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5])) #[0, 2, 3]
 print(same_values([5, 1, -10], [5, 10, -10, 3, 5])) #"Invalid..."
 
+# Qn 10 
+# Create a function named reversed_list() that 
+# takes two lists of the same size as parameters named lst1 and lst2.
+
+# The function should return True if lst1 is the same as lst2 reversed. 
+# The function should return False otherwise.
+
+# For example, reversed_list([1, 2, 3], [3, 2, 1]) should return True.
+
+def reversed_list(lst1, lst2):
+    if len(lst1) != len(lst2):
+        return "Invalid inputs: list length not same"
+    
+    for i in range(len(lst1)):
+        if lst1[i] != lst2[-(i+1)]:
+            return False
+    return True
+
+# cannot do the following as will return True the instance element matches
+# BUT we want ALL to match --> then true, if sth no match --> false else i.e. all match True
+#   for i in range(len(lst1)):
+#       if lst1[i] == lst2[-(i+1)]:
+#           return True
+#     return False
+
+print(reversed_list([1, 2, 3], [3, 2, 1]))  # Output: True
+print(reversed_list([1, 2, 3], [1, 2, 3]))  # Output: False
+print(reversed_list([1, 5, 3], [3, 2, 1])) # Output: False
+print(reversed_list([1, 2, 3], [3, 2, 1, 0]))  # Output: Invalid inputs: list length not same
