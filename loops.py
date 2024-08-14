b7f93ce1a94ca4c53de7a5e3b658367f30b7ef9d
Vanitaa Rai <vanit.rai26@gmail.com>
2024-08-14 13:24:09 +0800

    2nd loops qn

--- loops.py
+++ loops.py
@@ -13,3 +13,18 @@ def divisible_by_ten(nums):
     
 print(divisible_by_ten([20, 25, 30, 35, 40]))
 
+# Qn 2
+# Create a function named add_greetings() which takes a list of strings named names as a parameter.
+
+# In the function, create an empty list that will contain each greeting. 
+# Add the string 'Hello, ' in front of each name in names and append the greeting to the list.
+
+# Return the new list containing the greetings.
+
+def add_greetings(names):
+    greetings = []
+    for name in names:
+        greetings.append("Hello, " + name)
+    return greetings
+
+print(add_greetings(["Owen", "Max", "Sophie"]))
