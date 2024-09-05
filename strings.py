5748ef86f173377cd2fdb35f035270bf24fdba60
Vanitaa Rai <vanit.rai26@gmail.com>
2024-09-06 00:49:22 +0800

    str qn10

--- strings.py
+++ strings.py
@@ -200,3 +200,18 @@ def make_spoonerism(word1, word2):
 
 print("--Q9--")
 print(make_spoonerism("Jelly", "Bean"))
+
+# Q 10
+
+# Create a function named add_exclamation that has one parameter named word. 
+# This function should add exclamation points to the end of word until word is 20 characters long.
+# If word is already at least 20 characters long, just return word.
+
+def add_exclamation(word):
+  while(len(word) < 20):
+    word += "!"
+  return word
+
+print("--Q10--")
+print(add_exclamation("Codecademy"))
+print(add_exclamation("Codecademy is the best place to learn"))
