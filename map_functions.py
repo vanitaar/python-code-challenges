fff936e6f8d687ac040a6937ee6f136ae0b36040
Vanitaa Rai <vanit.rai26@gmail.com>
2024-09-06 19:03:44 +0800

    map fn alt and best practice

--- map_functions.py
+++ map_functions.py
@@ -51,3 +51,39 @@ result = list(map(lambda x, y: x + y, list1, list2))
 
 print(result)  # [11, 22, 33]
 
+
+# Alternatives to map function
+
+# list comprehensions
+doubled = [x * 2 for x in numbers] 
+
+# for loops
+
+doubled = [] 
+
+for x in numbers: 
+   doubled.append(x * 2) 
+
+
+"""
+Use map() when:
+
+We need to apply a function to every item in an iterable.
+
+We want to transform data without explicitly writing a for loop.
+
+We’re working with large datasets and want to avoid creating intermediate lists.
+"""
+
+"""
+Best Pracices:
+
+Use map() when it makes your code more readable and concise.
+
+For complex operations, consider using a regular function instead of a lambda.
+
+Remember that map() returns an iterator. If you need a list, wrap it with list().
+
+When working with multiple iterables, 
+ensure they have the same length to avoid unexpected results.
+"""
