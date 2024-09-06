fd9763a0b1bb2c85aa2b0fb9dd3d2dd51cc87303
Vanitaa Rai <vanit.rai26@gmail.com>
2024-09-06 18:15:17 +0800

    classes 3

--- classes.py
+++ classes.py
@@ -18,12 +18,39 @@
 
 # 2 Add Bot logic
 
+# class DriveBot: 
+#     def __init__(self):
+#         print("robot created")
+#         self.motor_speed = 0
+#         self.direction = 0
+#         self.sensor_range = 0
+    
+#     def control_bot(self, new_speed, new_dir):
+#         self.motor_speed = new_speed
+#         self.direction = new_dir
+#         print("speed and direction changed")
+
+#     def adjust_sensor(self, new_sensor_range):
+#         self.sensor_range = new_sensor_range
+#         print("sensor range adjusted")        
+        
+# robot_1 = DriveBot()
+
+# robot_1.control_bot(10, 180)
+# robot_1.adjust_sensor(20)
+
+# print(robot_1.motor_speed)
+# print(robot_1.direction)
+# print(robot_1.sensor_range)
+
+# 3 Enhance constructor
+
 class DriveBot: 
-    def __init__(self):
+    def __init__(self, motor_speed = 0, direction = 180, sensor_range = 10):
+        self.motor_speed = motor_speed
+        self.direction = direction
+        self.sensor_range = sensor_range
         print("robot created")
-        self.motor_speed = 0
-        self.direction = 0
-        self.sensor_range = 0
     
     def control_bot(self, new_speed, new_dir):
         self.motor_speed = new_speed
@@ -32,13 +59,25 @@ class DriveBot:
 
     def adjust_sensor(self, new_sensor_range):
         self.sensor_range = new_sensor_range
-        print("sensor range adjusted")        
+        print("sensor range adjusted")  
         
 robot_1 = DriveBot()
+robot_1.motor_speed = 5
+robot_1.direction = 90
+robot_1.sensor_range = 10
 
-robot_1.control_bot(10, 180)
-robot_1.adjust_sensor(20)
+robot_2 = DriveBot(35, 75, 25)
 
-print(robot_1.motor_speed)
-print(robot_1.direction)
-print(robot_1.sensor_range)
+print(robot_2.motor_speed)
+print(robot_2.direction)
+print(robot_2.sensor_range)
+
+## Different ways to use constructor:
+# sensor_range defaults to 10
+test_bot_1 = DriveBot(10, 270) 
+# direction defaults to 180
+test_bot_2 = DriveBot(sensor_range = 20, motor_speed = 45) 
+# direction defaults to 180 and sensor_range defaults to 10
+test_bot_3 = DriveBot(50) 
+# all default values are used
+test_bot_4 = DriveBot() 
