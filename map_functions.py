a5aa2168b008e230d263a6a0c3ddf4b48a7d0132
Vanitaa Rai <vanit.rai26@gmail.com>
2024-09-06 19:03:18 +0800

    map fn uses

--- map_functions.py
+++ map_functions.py
@@ -15,3 +15,39 @@ doubled_numbers = map(double, numbers)
 
 print(doubled_numbers) # <map object at 0x000001623E000190>
 print(list(doubled_numbers)) # [2, 4, 6, 8, 10]
+
+
+# Converting strings to integers 
+
+str_nums = ['1', '2', '3', '4', '5'] 
+
+int_nums = list(map(int, str_nums)) 
+
+print(int_nums)  # [1, 2, 3, 4, 5] 
+
+# Finding the length of strings 
+
+words = ['apple', 'banana', 'cherry'] 
+
+word_lengths = list(map(len, words)) 
+
+print(word_lengths)  # [5, 6, 6] 
+
+# Using w lambda fn
+
+numbers_to_subtract = [1, 2, 3, 4, 5] 
+
+substracted = list(map(lambda x: x -1, numbers_to_subtract)) 
+
+print(substracted)  # [0, 1, 2, 3, 4]
+
+# Using multiple iterables
+
+list1 = [1, 2, 3] 
+
+list2 = [10, 20, 30] 
+
+result = list(map(lambda x, y: x + y, list1, list2)) 
+
+print(result)  # [11, 22, 33]
+
